e5aacddb49ced62778433865a082ef295ac2784d
ilyacoos <gril.work@gmail.com>
2018-09-22 01:37:49 +0300

    stat

--- index.py
+++ index.py
@@ -1,6 +1,27 @@
 #import cgi
 import webapp2
 
+class Stat(webapp2.RequestHandler):
+    def get(self):
+        try:
+            import sys
+            sys.path.insert(0, 'lib')
+            from bs4 import BeautifulSoup
+            from urllib2 import urlopen
+            
+
+            HTML = urlopen("http://pif.investfunds.ru/funds/54/detail/1/", data="date1    01.01.2018\ndate2    20.09.2018\nset_compare[5]    0").read()
+            soup = BeautifulSoup(HTML)
+            table = soup.find("div", attrs={"class":"tabs-inner tabs-inner-active"}).find_all("table", attrs={"class":"table-data"})[1].find_all("tr")
+            self.response.write("<html><body>")
+            self.response.write("<!--" + table + "-->")
+            for r in table:
+                self.response.write("%s, %s, %s <br />" % r.find_all("td"))
+                
+            self.response.write("</body></html>")
+
+        except Exception as e:
+            self.response.write(e.message)
 
 try:
     import sys
@@ -20,6 +41,7 @@ try:
         ('/a2/pif', PifApp),
         ('/a2/help', MainPage),
         ('/a2/req', Req),
+        ('/a2/stat', Stat),
         ('/a2/torrent/add', AddTorrent),
         ('/a2/torrent/set_stat', SetStat),
         ('/a2/torrent/get_stat', GetStat),
